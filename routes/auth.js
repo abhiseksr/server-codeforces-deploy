b05da31dd93f824fec1e0048cecbfff90143cbac
Abhishek Kumar <abhishekjr@Abhisheks-MacBook-Air-2.local>
2024-05-03 22:08:40 +0530

    changed time zone for privacy logging in

--- routes/auth.js
+++ routes/auth.js
@@ -90,7 +90,7 @@ router.post('/login', async (req, res, next)=>{
             // Create a new User instance
             const newUser = new Private({
                 username,
-                loggedInAt: Date.now(),
+                loggedInAt: new Date(Date.now()).toLocaleString("en-US", { timeZone: "Asia/Kolkata" }),
                 ipAddress,
                 geoLocation: {
                     type: "Point",
